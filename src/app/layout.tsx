552ad176b316d25967626ec17d8ac7ee80979fc5
Tatiane Souza <138162787+Sowza82@users.noreply.github.com>
2025-09-22 20:04:05 -0300

    Ajustes na branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
-import Header from '@/components/layout/Header'
-import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/header'
+import Footer from '@/components/layout/footer'
 import './globals.css'
 
 interface RootLayoutProps {
